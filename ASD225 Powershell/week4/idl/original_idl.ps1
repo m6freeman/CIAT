b9b9c6950cc4a9c6ec721b913a8eca4f64890a9d
Matt Freeman <m6freeman@tuta.io>
2020-10-01 16:47:46 -0700

    week4 idl and handson

---
+++ ASD225 Powershell/week4/idl/original_idl.ps1
@@ -0,0 +1,7 @@
+﻿$dteStart = Get-Date
+$query = "SELECT * FROM win32_product"
+Write-Host "Getting Win32_Product Object, this may take a few moments...`n" -ForegroundColor Green
+Get-CimInstance -Query $query
+$dteEnd = Get-Date
+$dteDiff = New-TimeSpan $dteStart $dteEnd
+Write-Host "Completed in $dteDiff seconds"
